4273a5b3c452d402f4d2e6b9cfb254fe883f58e2
Li Kang <likang.dev@outlook.com>
2025-08-15 16:08:37 +0800

    perf: optimize history preview rendering

--- src/lib/components/HistoryButton.svelte
+++ src/lib/components/HistoryButton.svelte
@@ -3,7 +3,7 @@
 	import { clickOutsideDropdown } from '$lib/attachments/clickOutside';
 	import { getHistory } from '$lib/history';
 
-	let { historySelected, key }: { historySelected: (his: string) => void; key?:string } = $props();
+	let { historySelected, key }: { historySelected: (his: string) => void; key?: string } = $props();
 
 	let detailsElement: HTMLDetailsElement;
 	let history = $state<string[]>([]);
@@ -29,18 +29,18 @@
 		ontoggle={handleToggle}
 		{@attach clickOutsideDropdown}
 	>
-		<summary class="btn btn-ghost btn-sm btn-square m-1">
+		<summary class="btn btn-ghost btn-sm btn-square">
 			<HistoryIcon class="size-[1.2em]" />
 		</summary>
 		<ul class="dropdown-content bg-base-100 rounded-box right-0 z-1 w-52 p-2 text-xs shadow-sm">
 			{#if history.length > 0}
-				{#each history as his (his)}
+				{#each history as his}
 					<li>
 						<button
-							class="hover:bg-base-content/10 w-full cursor-pointer rounded-sm px-3 py-1.5 text-start outline-hidden"
+							class="hover:bg-base-content/10 w-full rounded-sm px-3 py-1.5 text-start outline-hidden"
 							onclick={() => handleHistoryClick(his)}
 						>
-							<span class="line-clamp-2 break-all">{his}</span>
+							<span class="line-clamp-2 break-all">{his.slice(0, 100)}</span>
 						</button>
 					</li>
 				{/each}
