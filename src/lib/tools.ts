39d7462eb682f19c4e8b1d9e8dd106486dbd7967
Li Kang <likang.dev@outlook.com>
2025-10-18 21:22:52 +0800

    chore: remove unused tool icons from tools list

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -2,11 +2,8 @@ import JsonIcon from '$lib/components/icons/Json.svelte';
 import TimeIcon from '@lucide/svelte/icons/clock';
 import ColorIcon from '@lucide/svelte/icons/palette';
 import Base64Icon from '$lib/components/icons/Base64.svelte';
-import EscapeIcon from '$lib/components/icons/Escape.svelte';
 import PercentIcon from '@lucide/svelte/icons/percent';
-import RegexIcon from '@lucide/svelte/icons/regex';
 import JwtIcon from '$lib/components/icons/Jwt.svelte';
-import QrCodeIcon from '@lucide/svelte/icons/qr-code';
 
 export const allTools = [
 	{
@@ -31,28 +28,13 @@ export const allTools = [
 		icon: Base64Icon
 	},
 	{
-		href: '#',
-		title: 'Escape',
-		icon: EscapeIcon
-	},
-	{
 		href: '/url-encoding',
 		title: 'URL Encoding',
 		icon: PercentIcon
 	},
 	{
-		href: '#',
-		title: 'Regex',
-		icon: RegexIcon
-	},
-	{
 		href: '/jwt',
 		title: 'JWT',
 		icon: JwtIcon
-	},
-	{
-		href: '#',
-		title: 'QR Code',
-		icon: QrCodeIcon
 	}
 ];
