d18056ec1274abecc701814964e02566d44588eb
Li Kang <likang.dev@outlook.com>
2025-10-18 21:20:51 +0800

    chore: remove basecoat-css script from app.html and initialize in layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,18 @@
 	import NavBar from './NavBar.svelte';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(async () => {
+		// Suppress monkey patching of history API by basecoat (see node_modules/basecoat-css/dist/js/all.js)
+		window.history.__basecoatPatched = true;
+		// @ts-ignore
+		await import('basecoat-css/all');
+		(window as any).basecoat.initAll();
+		(window as any).basecoat.start();
+	});
 </script>
 
 <ModeWatcher />
