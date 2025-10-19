a1f368ca33eafd89706da670245d65ced2b63369
Li Kang <likang.dev@outlook.com>
2025-10-19 21:43:07 +0800

    chore: clean code

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -1,13 +1,13 @@
 import type { Tool } from './types';
 
-import jsonTool from '../routes/json/json';
-import datetimeTool from '../routes/datetime/datetime';
-import colorTool from '../routes/color/color';
-import base64Tool from '../routes/base64/base64';
-import jwtTool from '../routes/jwt/jwt';
-import urlEncodingTool from '../routes/url-encoding/url-encoding';
+import json from '../routes/json/json';
+import datetime from '../routes/datetime/datetime';
+import color from '../routes/color/color';
+import base64 from '../routes/base64/base64';
+import jwt from '../routes/jwt/jwt';
+import urlEncoding from '../routes/url-encoding/url-encoding';
 
-export const allTools = [jsonTool, datetimeTool, colorTool, base64Tool, jwtTool, urlEncodingTool];
+export const allTools = [json, datetime, color, base64, jwt, urlEncoding];
 
 export function defineTool<T>(tool: Tool<T>): Tool<T> {
 	return tool;
